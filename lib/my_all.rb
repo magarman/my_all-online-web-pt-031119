e5d66f10354e88f32e0feea372ebff34533343fd
Mari Garman <garman.mariana@gmail.com>
2019-03-24 21:53:52 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,8 +3,7 @@ require 'pry'
 def my_all?(collection)
   i = 0
   block_return_values = []
-  while i < collection.legth
-  
+  while i < collection.length
   block_return_values << yield(collection[i])
   i += 1
 end
